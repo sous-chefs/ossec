8be8bf6d1af648ab5acf99ef9d262c37ea4ea53e
jtimberman <joshua@opscode.com>
2012-09-01 16:29:35 -0600

    add test-kitchen support & increment version for dev

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures ossec"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.2"
+version          "1.0.3"
 
 depends "build-essential"
 
