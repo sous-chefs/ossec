75b00408b0877e2e32375a5b2635b83aad14f827
jtimberman <joshua@opscode.com>
2013-08-20 10:41:08 -0600

    change maintainer

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             "ossec"
-maintainer       "Opscode, Inc."
-maintainer_email "cookbooks@opscode.com"
+maintainer       "Joshua Timberman"
+maintainer_email "cookbooks@housepub.org"
 license          "Apache 2.0"
 description      "Installs/Configures ossec"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
