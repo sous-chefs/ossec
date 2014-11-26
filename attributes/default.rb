1ee7e2f97328822c8cafac60d425f90a40d58401
Mark Luntzel <mark@luntzel.com>
2014-11-26 11:38:35 -0800

    setting default ssh timeout to nil

--- attributes/default.rb
+++ attributes/default.rb
@@ -35,7 +35,7 @@ default['ossec']['data_bag']['ssh']        = "ssh"
 
 # server-only
 default['ossec']['server']['maxagents'] = 256
-default['ossec']['server']['scp']['timeout'] = 30
+default['ossec']['server']['scp']['timeout'] = nil
 
 # used to populate config files and preload values for install
 default['ossec']['user']['language'] = "en"
