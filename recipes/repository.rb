c152abe39c4c352bf61113704d6cf9bf23071001
James Le Cuirot <james.le-cuirot@yakara.com>
2015-09-07 17:11:15 +0100

    Fix repository recipe when lsb-release is missing on Debian
    lsb-release is pre-installed on the Debian basebox but wasn't present
    on our production servers. Ohai uses it to set the
    node['lsb']['codename'] attribute.

--- recipes/repository.rb
+++ recipes/repository.rb
@@ -21,10 +21,18 @@ case node['platform_family']
 when 'fedora', 'rhel'
   include_recipe 'yum-atomic'
 when 'debian'
+  package 'lsb-release'
+
+  ohai 'reload lsb' do
+    plugin 'lsb'
+    action :nothing
+    subscribes :reload, 'package[lsb-release]', :immediately
+  end
+
   apt_repository 'ossec' do
     uri 'http://ossec.wazuh.com/repos/apt/' + node['platform']
     key 'http://ossec.wazuh.com/repos/apt/conf/ossec-key.gpg.key'
-    distribution node['lsb']['codename']
+    distribution lazy { node['lsb']['codename'] }
     components ['main']
   end
 end
