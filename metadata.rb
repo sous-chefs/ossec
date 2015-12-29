405a980c5b67fa849541c6fc6dfe5226cb07613b
Tim Smith <tsmith84@gmail.com>
2015-12-29 15:51:26 -0800

    Add supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'ossec'
 maintainer       'Joshua Timberman'
 maintainer_email 'cookbooks@housepub.org'
 license          'Apache 2.0'
-description      'Installs/Configures ossec'
+description      'Installs and onfigures ossec'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.5'
 
@@ -13,3 +13,6 @@ end
 %w( debian ubuntu arch redhat centos fedora scientific oracle amazon ).each do |os|
   supports os
 end
+
+source_url 'https://github.com/jtimberman/ossec-cookbook' if respond_to?(:source_url)
+issues_url 'https://github.com/jtimberman/ossec-cookbook/issues' if respond_to?(:issues_url)
