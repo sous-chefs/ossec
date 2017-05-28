6c32030742abdfc53ea47c08541d24612588521d
Tim Smith <tsmith@chef.io>
2017-05-28 13:24:45 -0700

    Update copyrights
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/authd.rb
+++ recipes/authd.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: ossec
 # Recipe:: authd
 #
-# Copyright 2015, Opscode, Inc.
+# Copyright 2015-2017, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
