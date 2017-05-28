10ce2b41bdabb7df979b4841e58ddd29a86c1d4b
Tim Smith <tsmith@chef.io>
2017-05-28 13:48:29 -0700

    Fix the chefspec deprecation warnings
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,4 +4,6 @@ require 'chefspec/berkshelf'
 RSpec.configure do |config|
   config.color = true               # Use color in STDOUT
   config.formatter = :documentation # Use the specified formatter
+  config.platform = 'ubuntu'
+  config.version = '16.04'
 end
