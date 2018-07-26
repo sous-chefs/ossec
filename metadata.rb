65b4af8ff42772b1fa5688193e704a038c65c6b2
Paul Welch <pwelch@chef.io>
2018-07-26 14:41:34 -0400

    Fix FC116
    Cookbook depends on the deprecated compat_resource cookbook
    
    Chef 12 is end of life and this cookbook wasn’t necessary after Chef 12.19.

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,8 @@ description      'Installs and configures ossec'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.5'
 
-depends 'compat_resource', '>= 12.14.6'
+chef_version '>= 12.19'
+
 depends 'yum-atomic'
 
 %w( debian ubuntu redhat centos fedora scientific oracle amazon ).each do |os|
@@ -15,4 +16,3 @@ end
 
 source_url 'https://github.com/sous-chefs/ossec'
 issues_url 'https://github.com/sous-chefs/ossec'
-chef_version '>= 12.5' if respond_to?(:chef_version)
