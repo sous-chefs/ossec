c54841d4276e8fd72de92d9d7cef4941782f3ed1
Mark E. Schill <Mark.Schill@cmschill.net>
2019-10-19 12:57:11 -0400

    Closes #122

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Sous Chefs'
 maintainer_email 'help@sous-chefs.org'
 license          'Apache-2.0'
 description      'Installs and configures ossec'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
 version          '1.2.0'
 chef_version     '>= 13.0'
 
