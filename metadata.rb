6f20d94b0a3e935dc7618cfa92b3d9f71d398bd8
Dan Webb <dan.webb@damacus.io>
2023-05-03 10:04:16 +0100

    Update metadata';ggp

--- metadata.rb
+++ metadata.rb
@@ -3,13 +3,18 @@ maintainer       'Sous Chefs'
 maintainer_email 'help@sous-chefs.org'
 license          'Apache-2.0'
 source_url       'https://github.com/sous-chefs/ossec'
-issues_url       'https://github.com/sous-chefs/ossec'
+issues_url       'https://github.com/sous-chefs/ossec/issues'
 description      'Installs and configures ossec'
 version          '2.0.4'
 chef_version     '>= 13.0'
 
 depends 'yum-atomic'
 
-%w( debian ubuntu redhat centos fedora scientific oracle amazon ).each do |os|
-  supports os
-end
+supports 'debian'
+supports 'ubuntu'
+supports 'redhat'
+supports 'centos'
+supports 'fedora'
+supports 'scientific'
+supports 'oracle'
+supports 'amazon'
